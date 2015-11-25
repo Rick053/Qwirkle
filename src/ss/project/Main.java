07054b10d30105ed9a58c8acbb034c8f15429f03
Rick053 <rvangemjr@gmail.com>
2015-11-25 15:03:17 +0100

    initial commit

---
+++ src/ss/project/Main.java
@@ -0,0 +1,25 @@
+package ss.project;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Qwirkle");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout, 500, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
