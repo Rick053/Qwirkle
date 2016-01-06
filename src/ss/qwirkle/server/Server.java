afa0a4037be3b27a22e370def78df9b397c296a6
Raoul Fasel <raoul@fasel.nl>
2016-01-06 14:38:03 +0100

    added cli and added cli to server

--- src/ss/qwirkle/server/Server.java
+++ src/ss/qwirkle/server/Server.java
@@ -3,17 +3,18 @@ package ss.qwirkle.server;
 import ss.qwirkle.common.ui.GUI;
 import ss.qwirkle.common.ui.TUI;
 import ss.qwirkle.common.ui.UserInterface;
-
+import ss.qwirkle.common.Cli
 public class Server {
 
     public static void main(String[] args) {
         UserInterface ui;
-
-        if(args.length > 0 && args[0].equals("-TUI")) {
-            ui = new TUI();
-        } else {
-            ui = new GUI();
-        }
+        new Cli(args,1).parseServer();
+        
+//        if(args.length > 0 && args[0].equals("-TUI")) {
+//            ui = new TUI();
+//        } else {
+//            ui = new GUI();
+//        }
 
         ui.run(args);
     }
