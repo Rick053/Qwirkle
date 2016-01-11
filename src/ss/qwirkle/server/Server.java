82fc76f7d41e53b9e30f4fa3d2d91835287c293b
Raoul Fasel <raoul@fasel.nl>
2016-01-11 11:02:10 +0100

    added cli with parsing off commands

--- src/ss/qwirkle/server/Server.java
+++ src/ss/qwirkle/server/Server.java
@@ -3,18 +3,18 @@ package ss.qwirkle.server;
 import ss.qwirkle.common.ui.GUI;
 import ss.qwirkle.common.ui.TUI;
 import ss.qwirkle.common.ui.UserInterface;
-import ss.qwirkle.common.Cli
+import ss.qwirkle.common.cli.Cli;
 public class Server {
 
     public static void main(String[] args) {
         UserInterface ui;
-        new Cli(args,1).parseServer();
-        
-//        if(args.length > 0 && args[0].equals("-TUI")) {
-//            ui = new TUI();
-//        } else {
-//            ui = new GUI();
-//        }
+        Cli cli = new Cli(args, 0);
+        cli.parse();
+        if (cli.getUI() == "TUI"){
+            ui = new TUI();
+        } else {
+            ui = new GUI();
+        }
 
         ui.run(args);
     }
