40dfe3a45e625376030d684961e991d870cdbb92
Rick053 <rvangemjr@gmail.com>
2016-01-14 13:35:25 +0100

    added main screen

---
+++ src/ss/qwirkle/server/controllers/MainController.java
@@ -0,0 +1,19 @@
+package ss.qwirkle.server.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    public ListView connections, messages;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
