163b2769b49c532a92375b465e8d12f14887a09b
Raoul Fasel <raoul@fasel.nl>
2016-01-17 18:49:53 +0100

    added row and collum add methods, still not tested at all:)

--- src/ss/qwirkle/common/Board.java
+++ src/ss/qwirkle/common/Board.java
@@ -7,10 +7,13 @@ import java.util.List;
  * Created by raoul on 17/01/16.
  */
 public class Board {
+    private int offSetY;
     List<List<Integer>> boardList = new ArrayList<>();
+    private int offSetX;
+
     public Board(int size){
-        List<Integer> x = new ArrayList<Integer>();
         for(int i = 0; i < size; i++) {
+            List<Integer> x = new ArrayList<Integer>();
             for (int j = 0; j < size; j++) {
                 x.add(0);
             }
@@ -20,15 +23,31 @@ public class Board {
     }
 
 
-    public void addTile(int x, int y){
+    public void addTile(int x, int y, int tile){
+        offSetY = boardList.size()/2;
+        offSetX = boardList.get(0).size()/2;
 
+        if ((x+offSetX)>boardList.size()){
+            addColumns();
+        }
 
+        if ((y+offSetY)>boardList.get(0).size()){
+            addRows();
+        }else boardList.get(y+offSetY).set(x+offSetX,tile);
     }
     public void addRows(){
-
+        List<Integer> x = new ArrayList<>();
+        for (int j = 0; j < boardList.get(0).size(); j++) {
+            x.add(0);
+        }
+        boardList.add(0,x);
+        boardList.add(x);
     }
-    public void addColumns{
-
+    public void addColumns(){
+        for (int j = 0; j < boardList.size(); j++){
+            boardList.get(j).add(0,0);
+            boardList.get(j).add(0);
+        }
     }
     @Override
     public String toString(){
