5beb7b63677fec3d0f895c29eb655142d701e493
Rick053 <rvangemjr@gmail.com>
2016-01-18 16:06:10 +0100

    added overview of chars for the colour and shape

--- src/ss/qwirkle/common/game/Tile.java
+++ src/ss/qwirkle/common/game/Tile.java
@@ -1,18 +1,37 @@
 package ss.qwirkle.common.game;
 
-import ss.qwirkle.common.Protocol;
-
 public class Tile {
 
+    /**
+     * Chars for the shape and colours:
+     * A - RED      || Circle
+     * B - ORANGE   || CROSS
+     * C - YELLOW   || DIAMOND
+     * D - GREEN    || SQUARE
+     * E - BLUE     || STAR
+     * F - PURPLE   || PLUS
+     */
     private char shape, colour;
+
+    private int row, column;
 
     public Tile() {
         this('0', '0');
     }
 
     public Tile(char shape, char colour) {
+        this(shape, colour, -1, -1);
+    }
+
+    public Tile(int row, int column) {
+        this('0', '0', row, column);
+    }
+
+    public Tile(char shape, char colour, int row, int column) {
         this.shape = shape;
         this.colour = colour;
+        this.row = row;
+        this.column = column;
     }
 
     @Override
@@ -22,6 +41,9 @@ public class Tile {
         return (t2.getShape() == shape && t2.getColour() == colour);
     }
 
+    /*
+     * Getters and Setters
+     */
     public void setColour(char colour) {
         this.colour = colour;
     }
@@ -36,5 +58,21 @@ public class Tile {
 
     public char getShape() {
         return shape;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
     }
 }
