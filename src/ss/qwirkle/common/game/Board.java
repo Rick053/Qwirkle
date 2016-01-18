950d2c2f17f65c5d157d92e2731df6e3d1400e7d
Rick053 <rvangemjr@gmail.com>
2016-01-18 16:01:57 +0100

    adapted the board class to work with tiles instead of ints

---
+++ src/ss/qwirkle/common/game/Board.java
@@ -0,0 +1,109 @@
+package ss.qwirkle.common.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    List<List<Tile>> boardList = new ArrayList<>();
+    private int offSetX, offSetY;
+
+    public Board(int size){
+        for(int i = 0; i < size; i++) {
+            List<Tile> col = new ArrayList<>();
+
+            for (int j = 0; j < size; j++) {
+                col.add(new Tile(j, i));    //Place empty tiles on all of the spots
+            }
+
+            boardList.add(col);
+        }
+
+        offSetX = size / 2;
+        offSetY = size / 2;
+    }
+
+    /**
+     * Add a new tile to the board.
+     * @param col
+     * @param row
+     * @param tile
+     */
+    public void addTile(int col, int row, Tile tile){
+        if (col + offSetX > boardList.size()) {
+            addColumns();
+        }
+
+        if (row + offSetY < boardList.get(0).size()) {
+            addRows();
+        }
+
+        boardList.get(row + offSetY).set(col + offSetX, tile);
+    }
+
+    /**
+     * Add extra rows to the board to make more space
+     */
+    public void addRows(){
+        List<Tile> row = new ArrayList<>();
+        for (int i = 0; i < boardList.get(0).size(); i++) {
+            row.add(new Tile());
+        }
+        boardList.add(0, row);  //Add extra row at the top
+        boardList.add(row);     //Add extra row at the bottom
+
+        offSetX = boardList.size() / 2; //Update the X offset
+    }
+
+    /**
+     * Add extra rows to the bottom to make more space
+     */
+    public void addColumns(){
+        for (int j = 0; j < boardList.size(); j++){
+            boardList.get(j).add(0, new Tile());    //Add column left
+            boardList.get(j).add(new Tile());       //Add column right
+        }
+
+        offSetY = boardList.get(0).size() / 2;      //Update the Y offset
+    }
+
+    @Override
+    public String toString(){
+        String board = "";
+        for (List<Tile> row : boardList) {
+
+            for (Tile tile : row) {
+                board += tile.toString();
+            }
+
+            board += System.lineSeparator();
+        }
+        return board;
+    }
+
+    //TODO remove intToQwirkle?
+    public String intToQwirkle(int i){
+        switch (i){
+            case -1:
+                return "#";
+            case 0:
+                return "O";
+            case 1:
+                return "\u25A0"; //square
+            case 2:
+                return "\u274C"; //cross
+            case 3:
+                return "\u271A"; //plus
+            case 4:
+                return "\u2737"; //star
+            case 5:
+                return "5";
+            case 6:
+                return "6";
+
+        }
+        return null;
+    }
+
+
+}
