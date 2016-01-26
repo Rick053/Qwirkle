8dccdc1005096c335350032c677a1a8d78900532
Rick053 <rvangemjr@gmail.com>
2016-01-26 14:49:31 +0100

    started determinemove

--- src/qwirkle/game/HumanPlayer.java
+++ src/qwirkle/game/HumanPlayer.java
@@ -1,7 +1,17 @@
 package qwirkle.game;
 
 import qwirkle.controllers.ClientController;
+import qwirkle.controllers.ServerController;
+import qwirkle.io.PrintColorWriter;
 import qwirkle.network.ClientHandler;
+import qwirkle.utils.Utils;
+import qwirkle.validation.InRange;
+import qwirkle.validation.Numeric;
+import qwirkle.validation.Validator;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 
 /**
  * Human player
@@ -35,9 +45,46 @@ public class HumanPlayer extends Player {
     @Override
     public Move determineMove() {
         ClientController controller = ClientController.getInstance();
+        String action;
 
-        controller.getUI().message("Choose a tile to place on the board.");
+        do {
+            Validator range = new InRange(0, 1, "Not a valid action.");
+            action = controller.getUI().getValidatedInput("What do you want to do? [0. Place Tile, 1. Submit Move] ",
+                    new Validator[] {range});
 
-        return new Move(); //TODO
+            if(action.equals("0")) {
+                placeTile();
+            }
+        } while(action.equals("0") && action != null);
+
+        return new Move();
+    }
+
+    private void placeTile() {
+        ServerController controller = ServerController.getInstance();
+
+        //TODO show board;
+        showHand();
+
+        Validator num = new Numeric("Please enter a number");
+        Validator range = new InRange(0, getHand().size() - 1, "Your choice was not a valid tile in your hand.");
+        String c = controller.getUI().getValidatedInput("", new Validator[] {num, range});
+        int choice = Utils.toInt(c);
+//
+        Tile chosenTile = getHand().get(choice);
+//        ArrayList<Point> possibilities = getGame().getBoard().findOptionsFor(chosenTile);
+    }
+
+    private void showHand() {
+        PrintColorWriter writer = ClientController.getInstance().getUI().getWriter();
+
+        for(int i = 0; i < getHand().size(); i++) {
+            Tile t = getHand().get(i);
+            writer.print(i + ": ");
+            writer.print(t.getColor(), t.getShape().toString());
+            writer.print(" - ");
+        }
+
+        writer.println("");
     }
 }
